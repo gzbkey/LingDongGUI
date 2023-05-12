c8c13c275ac35c88e436c41ff703c694503e34c2
gzbkey <59935554@qq.com>
2023-05-12 13:52:37 +0800

    keil fast model cm55添加路径
    Signed-off-by: gzbkey <59935554@qq.com>

--- examples/keil_fastModel/RTE/_CM55/Pre_Include_Global.h
+++ examples/keil_fastModel/RTE/_CM55/Pre_Include_Global.h
@@ -10,7 +10,7 @@
 #ifndef PRE_INCLUDE_GLOBAL_H
 #define PRE_INCLUDE_GLOBAL_H
 
-/* ARM::Acceleration:Arm-2D:Core:1.1.2-dev */
+/* ARM::Acceleration:Arm-2D:Core:1.1.3 */
 #define ___ARM_2D_CFG_HEADER___
 /* GorgonMeducer::Utilities:perf_counter:Core:Source:2.1.0 */
 #define __PERF_COUNTER_CFG_USE_SYSTICK_WRAPPER__ 1
